ae8c2ef769640b0297484f6e61605fecda1408e0
Jakob Krummeich <seamusjack@web.de>
2021-06-29 16:03:40 +0200

    Increased MAX_VERLET_DIST to improve performance.

--- general_code/global_variables.h
+++ general_code/global_variables.h
@@ -6,7 +6,7 @@
 using namespace std;
 
 constexpr int DIMENSION = 2;
-constexpr int TOTAL_NUMBER_OF_PARTICLES = 1000;
+constexpr int TOTAL_NUMBER_OF_PARTICLES = 16000;
 
 constexpr double CUTOFF = 2.5;
 constexpr double CUTOFF_SQUARED = CUTOFF * CUTOFF;
@@ -14,7 +14,7 @@ constexpr double INVERSE_CUTOFF = 1.0/CUTOFF;
 constexpr double POTENTIAL_CONSTANT_1 = pow(INVERSE_CUTOFF, 6.0) - pow(INVERSE_CUTOFF, 12.0);
 constexpr double POTENTIAL_CONSTANT_2 = 6.0 * pow(INVERSE_CUTOFF, 7.0) - 12.0 * pow(INVERSE_CUTOFF, 13.0);
 
-constexpr double MAX_VERLET_DIST = 1.3*CUTOFF;
+constexpr double MAX_VERLET_DIST = 1.4*CUTOFF;
 constexpr double MAX_VERLET_DIST_SQUARED = MAX_VERLET_DIST * MAX_VERLET_DIST;
 constexpr double SKINDISTANCE = MAX_VERLET_DIST - CUTOFF;
 
