af47865cecbb65c2fb398a9d6a9c0c5867fa9098
Jakob Krummeich <seamusjack@web.de>
2021-07-01 17:01:27 +0200

    Added cluster scripts to gitignore. Updated simulations info.

--- scripts/move_data_files_sgcmc.sh
+++ scripts/move_data_files_sgcmc.sh
@@ -17,16 +17,16 @@ function make_directory_if_necessary() {
 
 for filename in $sourcepath/*${included_pattern}*.dat; do
 
-		[[ ${filename} =~ _epsAB=([[:digit:]]*.[[:digit:]]*). ]] && epsAB=${BASH_REMATCH[1]}
-		currentpath=${targetpath}/epsAB=$epsAB
-		make_directory_if_necessary currentpath
+#		[[ ${filename} =~ _epsAB=([[:digit:]]*.[[:digit:]]*). ]] && epsAB=${BASH_REMATCH[1]}
+#		currentpath=${targetpath}/epsAB=$epsAB
+#		make_directory_if_necessary currentpath
 
-		[[ ${filename} =~ _AvgDens=([[:digit:]]*.[[:digit:]]*)_ ]] && density=${BASH_REMATCH[1]}
-		currentpath=${currentpath}/Roh=$density
-		make_directory_if_necessary currentpath
+#		[[ ${filename} =~ _AvgDens=([[:digit:]]*.[[:digit:]]*)_ ]] && density=${BASH_REMATCH[1]}
+#		currentpath=${currentpath}/Roh=$density
+#		make_directory_if_necessary currentpath
 
 		[[ ${filename} =~ _N=([[:digit:]]*)_ ]] && number_of_particles=${BASH_REMATCH[1]}
-		currentpath=${currentpath}/N=$number_of_particles
+		currentpath=${targetpath}/N=$number_of_particles
 		make_directory_if_necessary currentpath
 		
 		[[ ${filename} =~ _T=([[:digit:]]*.[[:digit:]]*)_ ]] && temperature=${BASH_REMATCH[1]}
