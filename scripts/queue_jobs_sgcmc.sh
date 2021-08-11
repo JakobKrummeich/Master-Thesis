50447d65396cb6410af500a4796b6f7299d3770b
Jakob Krummeich <seamusjack@web.de>
2021-08-11 12:13:04 +0200

    Updated run documentation as well as some scripts.

--- scripts/queue_jobs_sgcmc.sh
+++ scripts/queue_jobs_sgcmc.sh
@@ -7,10 +7,10 @@ settings="#!/bin/bash
 #SBATCH --mem-per-cpu=800mb
 #SBATCH --workdir=/home1/krummeich/Master-Thesis/simulation/semi_gcmc_no_field"
 
-for N in {500,1000,2000,4000,8000,16000}; do
+for N in 16000; do
 
-	for temperature in {0.74680,0.74690,0.74700,0.74710,0.74720}; do
-		for run_offset in {0,20,40,60}; do
+	for temperature in {0.78,0.79,0.80,0.81,0.82,0.83,0.84}; do
+		for run_offset in 0; do
 			echo "$settings" > N=${N}_T=${temperature}_${run_offset}.sh
 			echo  -e  >> N=${N}_T=${temperature}_${run_offset}.sh
 			srun_command="srun --ntasks=1 --error=error_stream_output/N=${N}_%J.err ./semi_gcmc_single_temp_${N} ${temperature} /home1/krummeich/Master-Thesis/data/semi_gcmc_no_field/sorted_data/finite_size_scaling_roh=0.75_long_runs/N=${N}/T=0.750000/States_N=${N}_T=0.750000_AvgDens=0.750000_MCRuns=10000000_epsAB=0.100000.dat 1 ${run_offset} &
