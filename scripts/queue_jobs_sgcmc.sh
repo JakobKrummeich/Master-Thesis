fb37d220219aa6df5ab939d2899a2bbc202f29d8
Jakob Krummeich <seamusjack@web.de>
2021-08-31 11:16:05 +0200

    Adapted sgcmc to be able to start more different states in parallel and control this correctly.

--- scripts/queue_jobs_sgcmc.sh
+++ scripts/queue_jobs_sgcmc.sh
@@ -8,16 +8,16 @@ settings="#!/bin/bash
 #SBATCH --workdir=/home1/krummeich/Master-Thesis/simulation/semi_gcmc_no_field"
 
 states_to_skip_per_run="1"
-number_of_equilibration_sweeps="2000000"
+number_of_equilibration_sweeps="0"
 max_number_of_sweeps="20000000"
 
-for N in 16000; do
-	for temperature in {0.7468,0.7469,0.7470,0.7471,0.7472}; do
+for N in {500,1000,2000,4000,8000,16000}; do
+	for temperature in {0.746800,0.746900,0.747000,0.747100,0.747200}; do
 		for run_offset in {0,20,40,60}; do
 			submit_filename="N=${N}_T=${temperature}_${run_offset}.sh"
 			echo "$settings" > ${submit_filename}
 			echo  -e  >> ${submit_filename}
-			srun_command="srun --ntasks=1 --error=error_stream_output/N=${N}_%J.err ./semi_gcmc_single_temp_${N} ${temperature} "/home1/krummeich/Master-Thesis/data/semi_gcmc_no_field/sorted_data/finite_size_scaling_roh=0.75_long_runs/N=${N}/T=0.750000/States_N=${N}_T=0.750000_AvgDens=0.750000_MCRuns=10000000_epsAB=0.100000.dat" ${states_to_skip_per_run} ${run_offset} ${number_of_equilibration_sweeps} ${max_number_of_sweeps} &
+			srun_command="srun --ntasks=1 --error=error_stream_output/N=${N}_%J.err ./semi_gcmc_single_temp_${N} ${temperature} "/home1/krummeich/Master-Thesis/data/semi_gcmc_no_field/sorted_data/finite_size_scaling_roh=0.75_equilibrated_states/N=${N}/T=${temperature}/States_N=${N}_T=${temperature}_AvgDens=0.750000_EquilibrationTime=2850_epsAB=0.100000.dat" ${states_to_skip_per_run} ${run_offset} ${run_offset} ${number_of_equilibration_sweeps} ${max_number_of_sweeps} &
 
 wait"
 			echo "$srun_command" >> ${submit_filename}
