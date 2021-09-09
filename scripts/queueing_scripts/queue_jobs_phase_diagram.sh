e733df4aa971eb93cb515f9a09000ed5e9f9ea98
Jakob Krummeich <seamusjack@web.de>
2021-09-09 11:08:32 +0200

    Fixed wrong workdir for queueing phase diagram computation.

--- scripts/queueing_scripts/queue_jobs_phase_diagram.sh
+++ scripts/queueing_scripts/queue_jobs_phase_diagram.sh
@@ -5,7 +5,7 @@ settings="#!/bin/bash
 #SBATCH --output=/dev/null
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=800mb
-#SBATCH --workdir=/home1/krummeich/Master-Thesis/simulation/semi_gcmc_no_field"
+#SBATCH --workdir=/home1/krummeich/Master-Thesis/data_analysis/"
 
 for N in {16000,8000,4000,2000,1000,500}; do
 
@@ -13,7 +13,7 @@ for N in {16000,8000,4000,2000,1000,500}; do
 	echo "$settings" > ${submit_filename}
 
 	srun_command="srun --ntasks=1 --error=error_stream_output/phase_diagram_computation_N=${N}_%J.err ./compute_distributions_and_phase_diagram.sh /home1/krummeich/Master-Thesis/data/semi_gcmc_no_field/sorted_data/finite_size_scaling_roh=0.75_long_runs_2/N=${N} 0 &
-	
+
 wait"
 
 	echo "${srun_command}" >> ${submit_filename}
