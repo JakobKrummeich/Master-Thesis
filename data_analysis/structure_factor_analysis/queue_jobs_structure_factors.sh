49895686260da9f65155e6cdf61d10f2fdb61016
Jakob Krummeich <seamusjack@web.de>
2021-09-10 14:37:19 +0200

    Fixed queue_job_structure_factors.sh script.

--- data_analysis/structure_factor_analysis/queue_jobs_structure_factors.sh
+++ data_analysis/structure_factor_analysis/queue_jobs_structure_factors.sh
@@ -2,21 +2,28 @@ sourcepath=$1
 
 settings="#!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --partition=oip
+#SBATCH --partition=gpu
 #SBATCH --nodes=1-1
 #SBATCH --output=/dev/null
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=800mb
-#SBATCH --workdir=/home1/krummeich/Master-Thesis/data_analysis/"
+#SBATCH --workdir=/home1/krummeich/Master-Thesis/data_analysis/structure_factor_analysis/"
 
 
-submit_filename="compute_phase_diagram_N=${N}.sh"
-echo "$settings" > ${submit_filename}
+for temperature_dir in ${sourcepath}/T=*; do
+	[[ ${temperature_dir} =~ /T=([[:digit:]]*.[[:digit:]]*) ]] && temperature=${BASH_REMATCH[1]}
+	filename="StructureFactor_T=${temperature}.sh"
+	echo "$settings" > $filename
+	echo  -e  >> $filename
 
-srun_command="srun --ntasks=1 --error=error_stream_output/phase_diagram_computation_N=${N}_%J.err ./compute_distributions_and_phase_diagram.sh ${sourcepath} 0 &
+	state_filename=($temperature_dir/States*.dat)
+	[[ ${state_filename} =~ States_(.*_epsAB=[[:digit:]]*.[[:digit:]]*).dat ]] && extracted_file_info=${BASH_REMATCH[1]}
+
+	srun_command="srun --ntasks=1 --error=error_stream_output/StructureFactor_computation_T=${temperature}_%J.err ./structure_factor ${state_filename} ${extracted_file_info} 1.0 25.0 16000 20 &
 
 wait"
 
-echo "${srun_command}" >> ${submit_filename}
-sbatch ${submit_filename}
+	echo "$srun_command" >> $filename
+	sbatch $filename
+done
 
