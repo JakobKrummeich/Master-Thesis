066be165330cdde3d189bcc5a445527d106a81a9
Jakob Krummeich <seamusjack@web.de>
2022-02-11 16:38:30 +0100

    Added queueing file for analysis of NA histograms.

---
+++ data_analysis/NA_analysis/queue_jobs_NA_analysis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+settings="#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --partition=phi
+#SBATCH --nodes=1-1
+#SBATCH --output=/dev/null
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=800mb
+#SBATCH --workdir=/home1/krummeich/Master-Thesis/code/data_analysis/NA_analysis"
+
+sourcepath=$1
+
+submitFilename="computeNA_analysis.sh"
+srun_command="srun --ntasks=1 --error=./error_stream.err ./compute_distributions_and_phase_diagram.sh ${sourcepath} &
+wait"
+
+echo "$settings" > ${submitFilename}
+echo -e >> ${submitFilename}
+echo "$srun_command" >> ${submitFilename}
+sbatch ${submitFilename}
+rm ${submitFilename}
+
