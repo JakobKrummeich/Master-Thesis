4e24d32ab0743235ca87a4612dc1436af09be60c
Jakob Krummeich <seamusjack@web.de>
2022-02-17 09:30:08 +0100

    Fixed a bug in the script that summarizes data aggregation queueing.

--- data_analysis/avg_data_files/analyze_singleRunData.sh
+++ data_analysis/avg_data_files/analyze_singleRunData.sh
@@ -2,9 +2,9 @@
 
 sourcepath=$1
 
-./queue_jobs_avg_data_files.sh ${sourcepath} "avgStresses_ShearStressSeries.dat" "1" "1"
-./queue_jobs_avg_data_files.sh ${sourcepath} "avgStresses_xEdges.dat" "2" "5"
-./queue_jobs_avg_data_files.sh ${sourcepath} "avgVelocities.dat" "15" "0"
-./queue_jobs_avg_data_files.sh ${sourcepath} "energySeries.dat" "1" "3"
-./queue_jobs_avg_data_files.sh ${sourcepath} "Img22.dat" "1" "4"
+./queue_jobs_avg_data_files.sh ${sourcepath} "avgStresses_ShearStressSeries" "1" "1"
+./queue_jobs_avg_data_files.sh ${sourcepath} "avgStresses_xEdges" "2" "5"
+./queue_jobs_avg_data_files.sh ${sourcepath} "avgVelocities" "15" "0"
+./queue_jobs_avg_data_files.sh ${sourcepath} "energySeries" "1" "3"
+./queue_jobs_avg_data_files.sh ${sourcepath} "Img22" "1" "4"
 
