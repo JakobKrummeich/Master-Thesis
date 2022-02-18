e61bdc9fdd4a9a63434858697f590fed223fca0c
Jakob Krummeich <seamusjack@web.de>
2022-02-18 18:11:20 +0100

    Added computation of avgStresses_yEdges to analyze_singleRunData.sh

--- data_analysis/avg_data_files/analyze_singleRunData.sh
+++ data_analysis/avg_data_files/analyze_singleRunData.sh
@@ -4,6 +4,7 @@ sourcepath=$1
 
 ./queue_jobs_avg_data_files.sh ${sourcepath} "avgStresses_ShearStressSeries" "1" "1"
 ./queue_jobs_avg_data_files.sh ${sourcepath} "avgStresses_xEdges" "2" "5"
+./queue_jobs_avg_data_files.sh ${sourcepath} "avgStresses_yEdges" "2" "5"
 ./queue_jobs_avg_data_files.sh ${sourcepath} "avgVelocities" "15" "0"
 ./queue_jobs_avg_data_files.sh ${sourcepath} "energySeries" "1" "3"
 ./queue_jobs_avg_data_files.sh ${sourcepath} "Img22" "1" "4"
