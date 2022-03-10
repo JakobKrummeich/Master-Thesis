eba96b15e8a4fea3640427c0c2f071a652a168bc
Jakob Krummeich <seamusjack@web.de>
2022-03-10 12:30:43 +0100

    Adapted queueing of equilibration to allow for different particle numbers.

--- MDSimulation/equilibrate_SGC_MD/queue_jobs_MD_equilibrate_SGC.sh
+++ MDSimulation/equilibrate_SGC_MD/queue_jobs_MD_equilibrate_SGC.sh
@@ -12,11 +12,13 @@ temperature=$1
 number_of_runs=$2
 number_of_equilibration_sweeps=$3
 number_of_data_taking_sweeps=$4
+totalNumberOfParticles=$5
 
-datapath="data/MDSimulations/equilibrate_SGC_MD/N=1000/T=${temperature}" # relative to root directory of repository
+datapath="data/MDSimulations/equilibrate_SGC_MD/N=${totalNumberOfParticles}/T=${temperature}" # relative to root directory of repository
 
 new_directory="../../../${datapath}"
-
+make_directory_if_necessary new_directory
+new_directory="${new_directory}/singleRunData"
 make_directory_if_necessary new_directory
 
 settings="#!/bin/bash
@@ -25,21 +27,20 @@ settings="#!/bin/bash
 #SBATCH --nodes=1-1
 #SBATCH --output=/dev/null
 #SBATCH --error=/dev/null
-#SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-core=1
 #SBATCH --mem-per-cpu=800mb
 #SBATCH --workdir=/home1/krummeich/Master-Thesis/code/MDSimulation/equilibrate_SGC_MD"
 
-
+#for runNumber in {145,54}; do
 for (( runNumber=0; runNumber<${number_of_runs}; runNumber++ )); do
-	submit_filename="equilibrate_SGC_MD_N=1000_T=${temperature}_${runNumber}.sh"
+	submit_filename="equilibrate_SGC_MD_N=${totalNumberOfParticles}_T=${temperature}_${runNumber}.sh"
 	echo "$settings" > ${submit_filename}
 	echo  -e  >> ${submit_filename}
 
 	result_directory="${new_directory}/${runNumber}/"
 	make_directory_if_necessary result_directory
 
-	srun_command="srun --ntasks=1 ./equilibrate_SGC_MD ${temperature} ${result_directory} ${number_of_equilibration_sweeps} ${number_of_data_taking_sweeps} &
+	srun_command="srun --ntasks=1 ./equilibrate_SGC_MD_${totalNumberOfParticles} ${temperature} ${result_directory} ${number_of_equilibration_sweeps} ${number_of_data_taking_sweeps} &
 
 wait"
 	echo "$srun_command" >> ${submit_filename}
