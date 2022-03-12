c5b5da1a1eb269eccfa338c7fafafb0cbf8a513b
Jakob Krummeich <seamusjack@web.de>
2022-03-12 12:07:31 +0100

    Adapted queueing of SGC_MD_shear for different number of particles.

--- MDSimulation/SGC_MD_shear/queue_jobs_SGC_MD_shear.sh
+++ MDSimulation/SGC_MD_shear/queue_jobs_SGC_MD_shear.sh
@@ -14,9 +14,10 @@ initStateDirectory=$3
 numberOfEquilibrationSweeps=$4
 numberOfDataTakingSweeps=$5
 stepsize=$6
+numberOfParticles=$7
 
 
-targetpath="data/MDSimulations/SGC_MD_shear/N=1000/T=${temperature}" # relative to root directory of repository
+targetpath="data/MDSimulations/SGC_MD_shear/N=${numberOfParticles}/T=${temperature}" # relative to root directory of repository
 
 newDirectory="../../../${targetpath}"
 make_directory_if_necessary newDirectory
@@ -47,20 +48,19 @@ settings="#!/bin/bash
 #SBATCH --workdir=/home1/krummeich/Master-Thesis/code/MDSimulation/SGC_MD_shear"
 
 
-
 for dir in ${initStateDirectory}*/ ; do
 
 	[[ ${dir} =~ T=[[:digit:]]*.[[:digit:]]*/([[:digit:]]*)/ ]] && runNumber="${BASH_REMATCH[1]}"
 
 	result_directory="${newDirectory}/${runNumber}/"
 
-	initialStateFile="${dir}N=1000_final_state.dat"
+	initialStateFile="${dir}N=${numberOfParticles}_final_state.dat"
 
-	srun_command="srun --ntasks=1 ./SGC_MD_shear ${temperature} ${shearRate} ${initialStateFile} ${result_directory} ${numberOfEquilibrationSweeps} ${numberOfDataTakingSweeps} ${stepsize} &
+	srun_command="srun --ntasks=1 ./SGC_MD_shear_N=${numberOfParticles} ${temperature} ${shearRate} ${initialStateFile} ${result_directory} ${numberOfEquilibrationSweeps} ${numberOfDataTakingSweeps} ${stepsize} &
 
 wait"
 
-	submit_filename="SGC_MD_shear_N=1000_T=${temperature}_shearRate=${shearRate}_${runNumber}.sh"
+	submit_filename="SGC_MD_shear_N=${numberOfParticles}_T=${temperature}_shearRate=${shearRate}_${runNumber}.sh"
 	echo "$settings" > ${submit_filename}
 	echo $'\n' >> ${submit_filename}
 	echo "$srun_command" >> ${submit_filename}
